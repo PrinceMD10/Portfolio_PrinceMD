e6f3e620fbaa8e674f1c05d5e6729639ce251df9
PrinceMD <princemdiomande@gmail.com>
2026-03-31 18:42:14 +0200

    add

--- src/components/sections/ProjectsSection.jsx
+++ src/components/sections/ProjectsSection.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useScrollAnimation } from '../../hooks/UseScrollAnimation';
+import { useScrollAnimation } from '../../hooks/useScrollAnimation';
 import { projects, projectCategories } from '../../data/projects';
 import ProjectCard from '../ui/ProjectCard';
 import "../../styles/ProjectsSection.css";
